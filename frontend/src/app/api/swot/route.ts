1588a8ceb30cfd385968e21f06861e88ff4813d5
feneel <feneeldoshi@gmail.com>
2025-07-18 16:36:07 -0400

    Frontend: Removing unused imports

--- frontend/src/app/api/swot/route.ts
+++ frontend/src/app/api/swot/route.ts
@@ -1,10 +1,4 @@
 import { NextResponse } from "next/server";
-import OpenAI from "openai";
-import { promptTemplates } from "@/libs/prompts";
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 
 export async function GET() {
